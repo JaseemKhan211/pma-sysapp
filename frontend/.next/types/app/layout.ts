3e41e5cf6fa851b95bbe804e9d2beee493f7daab
JaseemKhan211 <jskhan211@gmail.com>
2025-07-25 22:48:33 +0500

    Setup TailwindCSS manually with PostCSS for styling and added login UI

--- frontend/.next/types/app/layout.ts
+++ frontend/.next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: D:\pma-sysapp\frontend\src\app\layout.js
+// File: D:\pma-sysapp\frontend\src\app\layout.jsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
